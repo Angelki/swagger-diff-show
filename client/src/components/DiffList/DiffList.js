171eb2f2b5956c52196381178629b28ad648823f
Angelki <angelks4@live.com>
2018-04-25 15:37:52 +0800

    chamge state

--- client/src/components/DiffList/DiffList.js
+++ client/src/components/DiffList/DiffList.js
@@ -3,19 +3,64 @@ import { Table } from "antd";
 import axios from "axios";
 
 export default class DiffList extends Component {
+  state = {
+    infoMessages: [],
+    warningMessages: [],
+    errorMessages: [],
+    unmatchPaths: []
+  };
+
   componentWillMount() {
-    axios
-      .get("api/diffs/")
-      .then(res => console.log(res.data))
-      .catch(err => console.log(err));
+    this.onAxiosList();
   }
 
+  onAxiosList = () => {
+    axios
+      .get("api/diffs/")
+      .then(res => {
+        // * msg还是数组
+        let infoMsg = res.data.map(datalist =>
+          datalist.infos.map(item => item.message)
+        );
+        let warningMsg = res.data.map(datalist =>
+          datalist.warnings.map(item => item.message)
+        );
+        let errorMsg = res.data.map(datalist =>
+          datalist.errors.map(item => item.message)
+        );
+        let unmatchPathMsg = res.data.map(datalist =>
+          datalist.unmatchDiffs.map(item => item.path)
+        );
+        console.log(infoMsg);
+        console.log(warningMsg);
+        console.log(errorMsg);
+        console.log(unmatchPathMsg);
+
+        this.setState({
+          infoMessages: infoMsg,
+          warningMessages: warningMsg,
+          errorMessages: errorMsg,
+          unmatchPaths: unmatchPathMsg
+        });
+      })
+      .catch(err => console.log(err));
+  };
+
   render() {
+    const {
+      diffData,
+      infoMessages,
+      warningMessages,
+      errorMessages,
+      unmatchPaths
+    } = this.state;
+    console.log(infoMessages);
+    // console.log(typeof diffData);
+    // if (diffData !== []) console.log(diffData[0].infos.message);
     const columns = [
       {
         title: "Number",
-        dataIndex: "number",
-        render: text => <a href="javascript:;">{text}</a>
+        dataIndex: "number"
       },
       {
         title: "Infos",
@@ -28,15 +73,26 @@ export default class DiffList extends Component {
       },
       {
         title: "Errors",
-        dataIndex: "errors",
-        render: text => <a href="javascript:;">{text}</a>
+        dataIndex: "errors"
       },
       {
         title: "unmatchDiffs",
-        dataIndex: "unmatchDiffs",
-        render: text => <a href="javascript:;">{text}</a>
+        dataIndex: "unmatchDiffs"
       }
     ];
+    // let newList = [];
+    // diffData.map(item => {
+    //   let newItem = {
+    //     key: "test",
+    //     number: "API有改动",
+    //     infos: ,
+    //     warnings: ,
+    //     errors: ,
+    //     unmatchDiffs:
+    //   };
+    //   newList.unshift(newItem);
+    // });
+    // console.log(newList);
 
     const data = [
       {
